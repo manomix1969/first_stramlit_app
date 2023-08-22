6d9aebe874e6faf448d95785f87abfd7ee2e9a6c
manomix1969 <142856783+manomix1969@users.noreply.github.com>
2023-08-21 20:50:46 -0500

    Update streamlit_app.py
    added spinach

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.title('breakfast menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
